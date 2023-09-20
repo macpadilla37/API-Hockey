0e098de629cf4101931a9e1a62f8cdf9d5f22476
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-09-19 21:42:54 -0600

    Initial Commit

---
+++ API Hockey/API_HockeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_HockeyApp.swift
+//  API Hockey
+//
+//  Created by Mac Padilla on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct API_HockeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Hockey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API Hockey
+//
+//  Created by Mac Padilla on 9/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
