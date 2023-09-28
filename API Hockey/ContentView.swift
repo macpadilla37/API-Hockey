6961e2cfc45bbe36ab06c8f2f1560a8c12c5410b
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-09-28 14:59:21 -0600

    Update to Hockey

--- API Hockey/ContentView.swift
+++ API Hockey/ContentView.swift
@@ -18,20 +18,23 @@ struct Team: Codable, Identifiable {
     let id: Int
     let name: String
     let abbreviation: String
-    let city: String
+    let firstYearOfPlay: String
+    let officialSiteUrl: String
 }
 
 
 struct TeamsView: View {
+    let DarkRed = Color(red: 125/255, green: 23/255, blue: 23/255)
+    let lightBlue = Color(red: 215/255, green: 242/255, blue: 1)
+    let weirdOrange = Color(red: 255/255, green: 210/255, blue: 84/255)
     @State var NHL =  [Team]()
     @State var isLoading = false
-    @State private var showingSheet = false
     
     func getAllTeams() async {
         do {
             isLoading = true
             
-            let url = URL(string: "https://run.mocky.io/v3/3c4f07b8-2d8b-4c20-9fb4-812b2438768c")!
+            let url = URL(string: "https://statsapi.web.nhl.com/api/v1/teams")!
             let (data, _) = try await URLSession.shared.data(from: url)
             
             NHL = try JSONDecoder().decode(NationalHockeyLeague.self, from: data).teams
@@ -43,20 +46,45 @@ struct TeamsView: View {
     var body: some View {
         NavigationView {
             List(NHL) { team in
-                VStack(alignment: .leading) {
-                    Button(team.name){
-                            showingSheet = true
-                                    }
-                                    .sheet(isPresented: $showingSheet) {
-                                        SheetView()
-                                    }
-                                }
+                VStack(alignment: .leading){
+                    NavigationLink {
+                        ZStack {
+                            Color(DarkRed)
+                                .ignoresSafeArea()
+                        VStack{
+                            Text("Team:")
+                                .foregroundColor(Color(weirdOrange))
+                            Text(team.name)
+                                .padding(EdgeInsets(top: 0, leading: 10, bottom: 20, trailing: 10))
+                                .foregroundColor(Color(lightBlue))
+                            Text("Abbreviation:")
+                                .foregroundColor(Color(weirdOrange))
+                            Text(team.abbreviation)
+                                .padding(EdgeInsets(top: 0, leading: 10, bottom: 20, trailing: 10))
+                                .foregroundColor(Color(lightBlue))
+                            Text("First Year Team Played:")
+                                .foregroundColor(Color(weirdOrange))
+                            Text(team.firstYearOfPlay)
+                                .padding(EdgeInsets(top: 0, leading: 10, bottom: 20, trailing: 10))
+                                .foregroundColor(Color(lightBlue))
+                            Link("Site URL", destination: URL(string: team.officialSiteUrl)!)
+                            
+                        }
+                    }
+                    }label: {
+                        Text(team.name)
+                        
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .listRowBackground(Color(lightBlue))
             }
             .task {
                 await getAllTeams()
-            }
+                
+            }.foregroundColor(Color(DarkRed))
+            .navigationTitle("Teams")
         }
-        .navigationTitle("Team")
     }
 }
 
@@ -66,39 +94,3 @@ struct TeamView_Previews: PreviewProvider {
     }
 }
 
-struct SheetView: View {
-    @State var NHL =  [Team]()
-    @State var isLoading = false
-    @Environment(\.dismiss) var dismiss
-    
-    func getAllTeams() async {
-        do {
-            isLoading = true
-            
-            let url = URL(string: "https://run.mocky.io/v3/3c4f07b8-2d8b-4c20-9fb4-812b2438768c")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            NHL = try JSONDecoder().decode(NationalHockeyLeague.self, from: data).teams
-        } catch {
-            print("Error: \(error.localizedDescription)")
-        }
-    }
-    
-    
-    var body: some View {
-        NavigationView {
-            List(NHL) { team in
-                VStack(alignment: .leading) {
-                    Text(team.abbreviation)
-                    Text(team.city)
-                }
-                }
-            .task {
-                await getAllTeams()
-            }
-        }
-        Button("Press to Dismiss") {
-            dismiss()
-        }
-    }
-}
